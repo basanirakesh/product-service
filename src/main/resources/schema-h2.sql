3a77771ce031a57e8bc5d964d50e65f973781abd
Basani Rakesh <basanirakesh@gmail.com>
2020-08-07 13:37:13 +0200

    Adding CUSTOMER_PRODUCT table and loading dummy data into the tables

--- src/main/resources/schema-h2.sql
+++ src/main/resources/schema-h2.sql
@@ -10,5 +10,15 @@ create table PRODUCT
 (
  ID NUMBER(10) not null,
  TYPE VARCHAR2(30) not null,
- NAME VARCHAR2(50) not null
+ NAME VARCHAR2(50) not null,
+ INTEREST_RATE NUMBER(10) null,
+ PRIMARY KEY(ID)
+);
+
+create table CUSTOMER_PRODUCT
+(
+    CUSTOMER_ID NUMBER(10) not null,
+    PRODUCT_ID NUMBER(10) not null,
+    AMOUNT NUMBER(10) null,
+    PRIMARY KEY (CUSTOMER_ID, PRODUCT_ID)
 );
