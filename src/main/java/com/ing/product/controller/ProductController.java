1ca725936de5721a211b7d2613269e72857dcc17
Basani Rakesh <basanirakesh@gmail.com>
2020-08-07 12:08:38 +0200

    Adding actuator properties so that health can be seen

---
+++ src/main/java/com/ing/product/controller/ProductController.java
@@ -0,0 +1,11 @@
+package com.ing.product.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/product")
+public class ProductController {
+
+
+}
