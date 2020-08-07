71171d3036dc71f0b7897eb8f91cacde25713790
Sandipbhai Patel <sandippatelce@gmail.com>
2020-08-07 13:46:48 +0200

    Added beans.

---
+++ src/main/java/com/ing/product/bean/Product.java
@@ -0,0 +1,16 @@
+package com.ing.product.bean;
+
+import lombok.Data;
+import lombok.Builder;
+
+@Builder
+@Data
+public class Product {
+
+    private Long productId;
+    private String productName;
+    private ProductGroup productGroup;
+
+
+
+}
